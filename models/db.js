1e4c40638b69770d872d5d94198c1bb25fd647fb
Jasveen Kaur <jasveenkaur1299>
2022-04-04 15:36:12 +0530

    changed database

--- models/db.js
+++ models/db.js
@@ -1,8 +1,15 @@
 const mongoose = require('mongoose');
 
-mongoose.connect('mongodb://localhost:27017/Words_DB', { useNewUrlParser: true }, (err) => {
+// mongoose.connect('mongodb://localhost:27017/Words_DB', { useNewUrlParser: true }, (err) => {
+//     if (!err) { console.log('MongoDB Connection Succeeded.') }
+//     else { console.log('Error in DB connection : ' + err) }
+// });
+
+mongoose.connect('mongodb+srv://admin:admin@cluster0.ak1io.mongodb.net/WordsDB?retryWrites=true&w=majority', { useNewUrlParser: true }, (err) => {
     if (!err) { console.log('MongoDB Connection Succeeded.') }
     else { console.log('Error in DB connection : ' + err) }
 });
+
+
 
 require('./words.model');
